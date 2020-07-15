a61a0536efabe063c89955eb015721495916683b
byronbrown <68286443+byronbrown@users.noreply.github.com>
2020-07-15 15:38:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+example="example"
+print("This is an",example,"and has no errors.")
+
+name="Byron brown"
+fav_artist="lil uzi vert"
+vacation="florida"
+hobby="boxing & playing on my ps4"
+today="july/15/20"
+print("my name is","Byron brown","and my fav artist","Lil uzi vert")
+print("I love to go on vaction to","Florida ""and my two main hobbies are" "boxing and playing on my ps4")
+print("Today is","july/15/20")
